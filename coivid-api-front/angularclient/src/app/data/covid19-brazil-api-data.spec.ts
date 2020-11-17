3a171b8efb80dd210db54116ad97e9cffe97d9ee
Isaque Pestana <isaque.pestana@portaldedocumentos.com.br>
2020-11-17 13:45:44 -0300

    Ajustes finos

--- coivid-api-front/angularclient/src/app/data/covid19-brazil-api-data.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Covid19BrazilApiData } from './covid19-brazil-api-data';
-
-describe('Covid19BrazilApiData', () => {
-  it('should create an instance', () => {
-    expect(new Covid19BrazilApiData()).toBeTruthy();
-  });
-});
--- coivid-api-front/angularclient/src/app/data/covid19-brazil-api-data.ts
+++
@@ -1,7 +0,0 @@
-export class Covid19BrazilApiData {
-  country: string;
-  cases: number;
-  confirmed: number;
-  deaths: number;
-  recovered: number;
-}
